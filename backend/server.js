d73325110566bb75c93d5e3377f73166087aecec
z3tz3r0 <kittipan.wang@gmail.com>
2025-04-27 13:57:23 +0700

    fix(backend): tighten CORS policy

--- backend/server.js
+++ backend/server.js
@@ -13,22 +13,38 @@ connectDB();
 // Initialize express app
 const app = express();
 
-// Middleware BEFORE routes
-app.use(cors());
+// ---- Middleware BEFORE routes ----
+// using cors to specify
+const whiteList = [
+  "http://localhost:5173",
+  "https://ecommerce-space-craft.vercel.app/",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.warn(`CORS blocked for origin: ${origin}`);
+      callback(new Error("Not allowed"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
-// testing route
+// ---- Testing Route ----
 app.get("/", (req, res) => {
-    res.send("Spacekrub API Running");
+  res.send("Spacekrub API Running");
 });
 
+// ---- Routes ----
 // Mount Routers
 app.use("/api/products", productRoutes);
-
 // Define the port
 const PORT = process.env.PORT || 3000;
 
-// Start the server
+// ---- Start the server ----
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
